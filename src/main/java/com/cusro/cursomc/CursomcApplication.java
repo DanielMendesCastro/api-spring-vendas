0f592f1460414fe39d5d3a8531ff1b0abb1e6f63
Daniel <danielmendescastro@outlook.com>
2019-03-23 00:00:28 -0300

    Adicionando classe de Produto

--- src/main/java/com/cusro/cursomc/CursomcApplication.java
+++ src/main/java/com/cusro/cursomc/CursomcApplication.java
@@ -1,13 +1,52 @@
 package com.cusro.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cusro.cursomc.domain.Categoria;
+import com.cusro.cursomc.domain.Produto;
+import com.cusro.cursomc.repositories.CategoriaRepository;
+import com.cusro.cursomc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	@Autowired
+	private ProdutoRepository repoProduto;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat = new Categoria(null,"Informatica"); 
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		
+		Produto p1 = new Produto(null,"Computador",2000D);
+		Produto p2 = new Produto(null,"Impressora 800",150D);
+		Produto p3 = new Produto(null,"Mouse 80",35D);
+		
+		cat.setProdutos(Arrays.asList(p1,p2,p3));
+		cat2.setProdutos(Arrays.asList(p2));	
+		
+		p1.setCategorias(Arrays.asList(cat));;
+		p2.setCategorias(Arrays.asList(cat,cat2)); ;
+		p3.setCategorias(Arrays.asList(cat));
+		
+		repo.saveAll(Arrays.asList(cat,cat2));
+		repoProduto.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
--- src/main/java/com/cusro/cursomc/domain/Produto.java
+++ src/main/java/com/cusro/cursomc/domain/Produto.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto  implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
@@ -20,42 +22,52 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	private Double valor;
 	
-	public Categoria(Integer id, String nome) {
+	@ManyToMany
+	 @JoinTable(name = "PRODUTO_CATEGORIA",
+	 joinColumns = @JoinColumn(name = "produto_id"),
+	 inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	 )
+	private List<Categoria>  categorias = new ArrayList<>();
+	
+	public Produto() {
+	
+	}
+	
+	public Produto(Integer id, String nome, Double valor) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.valor = valor;
 	}
 	
-	public Categoria() {
-	}	
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
 	
-	public List<Produto> getProdutos() {
-		return produtos;
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
-	
 
 	@Override
 	public int hashCode() {
@@ -73,7 +85,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -81,4 +93,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
